92b6f65da8af6baa5127d49406f3878f52652110
Jae Hoon (Antonio) Kim <antoniok9130@users.noreply.github.com>
2017-05-15 21:49:12 -0400

    Add files via upload

--- Image Recognition/ImageConversion/ImageConversion.py
+++ Image Recognition/ImageConversion/ImageConversion.py
@@ -1,28 +1,11 @@
 import numpy as np
-import os.path
 
-combinations = []
-if (os.path.exists("./Combinations.npy")):
-    combinations = np.load("./Combinations.npy")
-else:
-    colour_range = range(0, 257)
-    logy = np.log(1367)
-    logz = np.log(16001)
-
-    for x in colour_range:
-        d1 = []
-        for y in colour_range:
-            d2 = []
-            for z in colour_range:
-                d2.append(np.log1p(x) + np.log1p(y) / logy + np.log1p(z) / logz)
-            d1.append(d2)
-        combinations.append(d1)
-
-    np.save("./Combinations.npy", combinations)
+logy = np.log(1367)
+logz = np.log(16001)
 
 def convert_image(input):
     length = len(input)
     if isinstance(input, list) and length > 0 and isinstance(input[0], tuple):
         def formula(i):
-            return combinations[i[0]][i[1]][i[2]]
+            return np.log1p(i[0])+np.log1p(i[1])/logy+np.log1p(i[2])/logz
         return map(formula, input)
--- Image Recognition/ImageConversion/Test.py
+++ Image Recognition/ImageConversion/Test.py
@@ -17,3 +17,6 @@ converted = ic.convert_image(input)
 end = int(round(time.time() * 1000))
 
 print("Data Processed.  Took", (end-start), "Milliseconds to process data.")
+
+for x,y in zip(input, converted):
+    print(x, " ", y)
